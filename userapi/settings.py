ee86867dde70da3b1702dc59a4f19adf65e523f6
fikayo06 <fikayomiopadotun@gmail.com>
2025-02-19 12:45:59 +0100

    deploy

--- userapi/settings.py
+++ userapi/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ei3711rrg-v#3-=aakjfg!(@$nzmu@=)^q10cbl7n2ocza#f0%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'https://diaryapp-3u87.onrender.com', '']
 
 
 # Application definition
